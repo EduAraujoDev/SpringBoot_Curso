b6344cf29a868c202af321c5963fbdc378236d50
EduAraujoDev <eduardo.araujo0@outlook.com>
2018-12-06 23:17:02 -0200

    Testando JPA com h2

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,22 @@
 package com.example.demo;
 
+import com.example.demo.entities.Empresa;
+import com.example.demo.repositories.EmpresaRepository;
 import com.example.demo.utils.SenhaUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class DemoApplication {
+
+	@Autowired
+	private EmpresaRepository empresaRepository;
 
 	@Value("${paginacao.qtd_por_pagina}")
 	private int qtdPorPagina;
@@ -27,8 +35,30 @@ public class DemoApplication {
 			System.out.println("Senha encoded novamente: " + senhaEncoded);
 
 			System.out.println("Senha válida: " + SenhaUtils.senhaValida("123456", senhaEncoded));
-
 			System.out.println("### Quantidade de elementos por página - " + this.qtdPorPagina);
+
+			// Teste de persistencia
+			Empresa empresa = new Empresa();
+			empresa.setRazaoSocial("Teste IT");
+			empresa.setCnpj("74645215000104");
+
+			this.empresaRepository.save(empresa);
+
+			List<Empresa> empresas = empresaRepository.findAll();
+			empresas.forEach(System.out::println);
+
+			Empresa empresaDb = empresaRepository.findById(1L).orElse(null);
+			System.out.println("Empresa por ID: " + empresaDb);
+
+			empresaDb.setRazaoSocial("Teste IT Web");
+			this.empresaRepository.save(empresaDb);
+
+			Empresa empresaCnpj = empresaRepository.findByCnpj("74645215000104");
+			System.out.println("Empresa por CNPJ: " + empresaCnpj);
+
+			this.empresaRepository.deleteById(1L);
+			empresas = empresaRepository.findAll();
+			System.out.println("Empresas: " + empresas.size());
 		};
 	}
 }
