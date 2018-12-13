13a6ef7551069d6d04fa904aa8c7860d80b3c28b
EduAraujoDev <eduardo.araujo0@outlook.com>
2018-12-13 21:36:34 -0200

    Testando flyway

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,7 @@
 package com.example.demo;
 
-import com.example.demo.entities.Empresa;
 import com.example.demo.repositories.EmpresaRepository;
-import com.example.demo.utils.SenhaUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,9 +15,6 @@ public class DemoApplication {
 	@Autowired
 	private EmpresaRepository empresaRepository;
 
-	@Value("${paginacao.qtd_por_pagina}")
-	private int qtdPorPagina;
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
@@ -28,37 +22,7 @@ public class DemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner() {
 		return args -> {
-			String senhaEncoded = SenhaUtils.geraBCrypt("123456");
-			System.out.println("Senha encoded: " + senhaEncoded);
 
-			senhaEncoded = SenhaUtils.geraBCrypt("123456");
-			System.out.println("Senha encoded novamente: " + senhaEncoded);
-
-			System.out.println("Senha válida: " + SenhaUtils.senhaValida("123456", senhaEncoded));
-			System.out.println("### Quantidade de elementos por página - " + this.qtdPorPagina);
-
-			// Teste de persistencia
-			Empresa empresa = new Empresa();
-			empresa.setRazaoSocial("Teste IT");
-			empresa.setCnpj("74645215000104");
-
-			this.empresaRepository.save(empresa);
-
-			List<Empresa> empresas = empresaRepository.findAll();
-			empresas.forEach(System.out::println);
-
-			Empresa empresaDb = empresaRepository.findById(1L).orElse(null);
-			System.out.println("Empresa por ID: " + empresaDb);
-
-			empresaDb.setRazaoSocial("Teste IT Web");
-			this.empresaRepository.save(empresaDb);
-
-			Empresa empresaCnpj = empresaRepository.findByCnpj("74645215000104");
-			System.out.println("Empresa por CNPJ: " + empresaCnpj);
-
-			this.empresaRepository.deleteById(1L);
-			empresas = empresaRepository.findAll();
-			System.out.println("Empresas: " + empresas.size());
 		};
 	}
 }
